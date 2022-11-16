a17ebdea53c32504c951c29bd274d71451f28c6a
Ivan <ivanwhitemac@gmail.com>
2022-11-16 18:08:11 +0000

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<header class="bg-teal-500 flex mx-auto justify-between p-4">
+			<nav>
+				<NuxtLink to="/">KeyWorld</NuxtLink>
+				<ul>
+					<li>
+						<NuxtLink to="/about">About</NuxtLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<div class="container mx-auto p-4">
+			<slot />
+		</div>
+		<footer class="container mx-auto p-4 flex justify-between bottom-0 fixed">
+			<p>Created by Ivan White | 2022</p>
+		</footer>
+	</div>
+</template>
+
+<style scoped>
+	.router-link-exact-active {
+		color: red;
+	}
+</style>
---
+++ pages/categories/[id].vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>{{ id }}</h1>
+		<h3>Product description:</h3>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const { id } = useRoute().params
+</script>
+
+<style scoped></style>
